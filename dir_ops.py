427f2ac5144869a970e119b615eaff5e6f76b92a
Kabbes <E150445@ameren.com>
2020-08-05 09:23:40 -0500

    adding path and copy functions

--- dir_ops.py
+++ dir_ops.py
@@ -1,1 +1,52 @@
 import shutil
+import pyperclip
+import os
+import sys
+
+backslash = '\\'
+
+def get_cwd():
+    '''returns current working directory'''
+    return os.getcwd()
+
+def cd_dot_dot(path):
+    '''goes back one level in the path'''
+
+    path_list = path.split(backslash)
+    back_one = join_dirs(path_list[:-1])
+    return back_one
+
+def list_contents(path):
+    '''shows all directories and files contained in path'''
+
+    return os.listdir(path)
+
+def path_exists(path):
+    '''returns boolean checking is path exists'''
+
+    return os.path.exists(path)
+
+def join_dirs(list_of_items):
+    '''joins directories with a backslash in between'''
+
+    return backslash.join(list_of_items)
+
+def copy_paste_file(copy_path, paste_path):
+    '''copies and pastes a file into a new location'''
+
+    shutil.copyfile(copy_path, paste_path)
+
+def remove_file(path, override = False):
+    '''removes file at given path'''
+
+    inp = 'delete'
+    if not override:
+        inp = input('Type "delete" to delete ' + str(path))
+
+    if inp == 'delete':
+        os.remove(path)
+
+def rename_file(og_path, new_path):
+    '''renames og to new'''
+
+    os.rename(og_path, new_path)
